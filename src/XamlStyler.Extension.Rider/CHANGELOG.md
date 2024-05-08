a62f248fc2f7afbeb02b668a58f43b5ea2fb1119
taivison <taivison@gmail.com>
2024-05-08 10:40:37 -0300

    XStyler .Net 8.0

--- src/XamlStyler.Extension.Rider/CHANGELOG.md
+++
@@ -1,1 +0,0 @@
-Check out our [release](https://github.com/Xavalon/XamlStyler/releases) page for a full history of changes.
--- src/XamlStyler.Extension.Rider/rider/main/kotlin/xavalon/plugins/xamlstyler/rider/XamlStylerOptionsPage.kt
+++
@@ -1,9 +0,0 @@
-package xavalon.plugins.xamlstyler.rider
-
-import com.jetbrains.rider.settings.simple.SimpleOptionsPage
-
-class XamlStylerOptionsPage
-    : SimpleOptionsPage("XAML Styler", "XamlStylerOptionsPage") {
-
-    override fun getId(): String = pageId + "Id"
-}
